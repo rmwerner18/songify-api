04535a75a1bc38f71ff38feeddef4b51b31bbc1c
humorous-storage-7521 <rmwerner@umich.edu>
2020-10-19 14:57:48 -0500

    make auth work

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,6 @@
 class SongsController < ApplicationController
+    skip_before_action :authorized, only: [:index, :show]
+    
     def index 
         songs = Song.all
         render json: songs
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-
     has_many :songs
     has_many :likes
+    validates :username, uniqueness: { case_sensitive: false }
 end
