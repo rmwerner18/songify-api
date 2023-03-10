512725c1aed784190b78eedabdcefa9a9a10c42b
humorous-storage-7521 <ryanwerner50@gmail.com>
2023-03-10 12:07:17 -0600

    remove fallback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::API
     before_action :authorized
-    skip_before_action :authorized, only: [:fallback_index_html]
-
-    def fallback_index_html
-      render '/'
-    end
      
     def encode_token(payload)
         # don't forget to hide your secret in an environment variable
--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,5 @@ Rails.application.routes.draw do
   post '/likes', to: 'likes#create'
   delete '/likes/:id', to: 'likes#destroy'
   resources :ws
-  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
-    !request.xhr? && request.format.html?
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
